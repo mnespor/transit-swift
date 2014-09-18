e93d4f1dfda3fc7781462bf447afc5d04d016d5b
mnespor <MattNespor@nanonation.net>
2014-09-17 22:14:12 -0500

    work on Set

--- Common/Set.swift
+++ Common/Set.swift
@@ -8,9 +8,38 @@
 
 import Foundation
 
-public struct Set<T : Hashable>
+public struct Set<T : Hashable> : SequenceType
 {
     private var buckets = [Int : [T]]()
+    public var count: Int {
+        get {
+            return self.allElements.count
+        }
+    }
+    
+    public var allElements: Array<T> {
+        get {
+            var result = Array<T>()
+            result.reserveCapacity(buckets.count)
+            for (key, bucket) in buckets
+            {
+                result += bucket
+            }
+            return result
+        }
+    }
+    
+    public var anyElement: T? {
+        var gen = self.generate()
+        return gen.next()
+    }
+    
+    public init() {
+    }
+    
+    public init<S: SequenceType where S.Generator.Element == T>(sequence: S) {
+        self.addElements(sequence)
+    }
     
     public func contains(element:T) -> Bool {
         if let bucket = buckets[element.hashValue] {
@@ -23,26 +52,104 @@ public struct Set<T : Hashable>
         return false
     }
     
-    public mutating func add(newElement:T) {
+    public mutating func addElement(newElement:T) {
         if !self.contains(newElement) {
-            if var bucket = buckets[newElement.hashValue] {
-                bucket.append(newElement)
+            let hash = newElement.hashValue
+            if buckets[hash] != nil {
+                buckets[hash]?.append(newElement)
             }
             else {
-                buckets[newElement.hashValue] = [newElement]
+                buckets[hash] = [newElement]
             }
+        }
+    }
+    
+    public mutating func addElements<S: SequenceType where S.Generator.Element == T> (sequence: S) {
+        for element in [T](sequence) {
+            self.addElement(element)
         }
     }
     
     public mutating func remove(element:T) {
-        if var bucket = buckets[element.hashValue] {
-            for var idx = 0; idx < bucket.count; idx++ {
-                if bucket[idx] == element {
-                    bucket.removeAtIndex(idx)
+        let hash = element.hashValue
+            for var idx = 0; idx < buckets[hash]?.count; idx++ {
+                if buckets[hash]?[idx] == element {
+                    buckets[hash]?.removeAtIndex(idx)
                     break
                 }
             }
-        }
     }
     
+    public func isSubsetOf(set:Set<T>) -> Bool {
+        for element in self {
+            if !set.contains(element) {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    public func intersects(set:Set<T>) -> Bool {
+        for element in self {
+            if set.contains(element) {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    // MARK: - SequenceType
+    
+    public func generate () -> GeneratorOf<T> {
+        var gen = SetGenerator(set: self)
+        return GeneratorOf() {
+            gen.next()
+        }
+    }
 }
+
+struct SetGenerator<T : Hashable> : GeneratorType
+{
+    var bucketsGenerator : DictionaryGenerator<Int, [T]>
+    var bucketGenerator : IndexingGenerator<[T]>?
+    
+    init (set:Set<T>) {
+        bucketsGenerator = set.buckets.generate()
+    }
+    
+    mutating func next() -> T? {
+        if let result = bucketGenerator?.next() {
+            return result
+        }
+        
+        while let (key, bucket) = bucketsGenerator.next() {
+            bucketGenerator = bucket.generate()
+            if let result = bucketGenerator?.next() {
+                return result
+            }
+        }
+        
+        return nil
+    }
+}
+
+// MARK: - Equatable
+
+public func ==<T:Hashable>(lhs: Set<T>, rhs: Set<T>) -> Bool {
+    return lhs.isSubsetOf(rhs) && rhs.isSubsetOf(lhs)
+}
+
+public func !=<T:Hashable>(lhs: Set<T>, rhs: Set<T>) -> Bool {
+    return !lhs.isSubsetOf(rhs) || !rhs.isSubsetOf(lhs)
+}
+
+// MARK: Union operations
+
+public func +<S: SequenceType> (set: Set<S.Generator.Element>, sequence: S) -> Set<S.Generator.Element> {
+    var result = set
+    result.addElements(sequence)
+    return result
+}
+
---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,63 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+import Transit
+
+var str = "Hello, playground"
+
+var mySet = Set<String>()
+
+mySet.addElement("bar")
+mySet.addElement("bar")
+mySet.addElement("bar")
+mySet.addElement("foo")
+mySet.addElement("qux")
+mySet.addElement("baz")
+mySet.addElement("quuuux")
+
+mySet.allElements
+
+mySet.remove("foo")
+
+mySet.allElements
+
+mySet.remove("foo")
+
+mySet.allElements
+
+mySet.contains("foo")
+mySet.contains("bleh")
+
+mySet.count
+
+var myGen = mySet.generate()
+myGen.next()
+myGen.next()
+myGen.next()
+myGen.next()
+myGen.next()
+myGen.next()
+
+
+let arr = [1, 2, 3, 4, 5]
+
+let truthiness = true && false
+
+//let newSet = mySet + ["a", "b", "c", "d", "qux"]
+
+mySet.addElements(["a", "b", "c", "qux"])
+
+mySet.intersects(Set(sequence: ["a", "what", "yeah"]))
+mySet.intersects(Set(sequence: ["okay", "what", "yeah"]))
+
+mySet.allElements
+
+let secondSet = Set(sequence: ["what", "okay", "yeah"])
+
+let otherSet = mySet + secondSet
+
+otherSet.allElements
+
+arr.debugDescription
+arr.description
+
